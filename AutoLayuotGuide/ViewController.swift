827fc98b112f3a8f3d46422cce2adf63e541580d
ViLkiS <thevilkis@gmail.com>
2023-03-07 10:30:13 +0400

    Initial Commit

---
+++ AutoLayuotGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayuotGuide
+//
+//  Created by Дмитрий Гусев on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
